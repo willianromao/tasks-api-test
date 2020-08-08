a4f6e8d9af22814c9e4aaae5acf647451b423c02
WillianRomao <willian.romao@outlook.com.br>
2020-08-08 14:14:20 -0300

    teste de get

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -21,56 +21,7 @@ public class APITest {
 			.get("/todo")
 		.then()
 			.statusCode(200)
-		;
-	}
-	
-	@Test
-	public void deveAdicionarTarefaComSucesso() {
-		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(201)
-		;
-	}
-	
-	@Test
-	public void naoDeveAdicionarTarefaInvalida() {
-		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(400)
-			.body("message", CoreMatchers.is("Due date must not be in past"))
-		;
-	}
-	
-	@Test
-	public void deveRemoverTarefaComSucesso() {
-		//inserir
-		Integer id = RestAssured.given()
-			.body("{ \"task\": \"Tarefa para remoção\", \"dueDate\": \"2020-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-//			.log().all()
-			.statusCode(201)
-			.extract().path("id")
-		;
-		
-		System.out.println(id);
-		
-		//remover
-		RestAssured.given()
-		.when()
-			.delete("/todo/"+id)
-		.then()
-			.statusCode(204)
+
 		;
 	}
 }
